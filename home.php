82fb76eed2132e55201d7d7602e6b8acf9042675
Its-PJ <81931176+Its-PJ@users.noreply.github.com>
2021-11-26 11:05:43 -0500

    Updated home and index files. Added css file

--- home.php
+++ home.php
@@ -1,23 +1,45 @@
 <!DOCTYPE html>
 <html>
-<body>
 
+<head><link href="styles.css" rel="stylesheet" type="text/css"></head>
+
+<body> 
+
+<!--
 <nav>
   <a href="home.php">Home</a>
   <a href="user.php">Add User</a>
   <a href="create.php">New Issue</a>
   <a href="logout.php">Logout</a>
 </nav>
+-->
 
-<br><br><header>BugMe Issue Tracker</header>
+<!--
+  <br><br><header>BugMe Issue Tracker</header>
+-->
 
-<h1>Issues</h1>
+<div  id="top">
+  <h1>Issues</h1>
+  <a href='create.php'><button type="button" class="create issue">Create New issue</button></a>
+</div>
+
 <br>
-<br>
-
-<a href='create.php'><button type="button" class="create issue">Create New issue</button></a>
+  <br>
 <!--The above button should redirect to create.php-->
 
+<div id="filters">
+  <p>Filter By:</p>
+
+  <button>All</button>
+
+  <button>Open</button>
+
+  <button>My Tickets</button>
+</div>
+
+<div id="issues table"> 
+
+</div>
 </body>
 </html>
 
--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
-<body>
 
+<head><link href="styles.css" rel="stylesheet" type="text/css"></head>
+
+<body>
 <header>BugMe Issue Tracker</header>
 
+<nav id="left-sidebar">
+  <a href="home.php" id="home">Home</a>
+  <a href="user.php" id="user">Add User</a>
+  <a href="create.php" id="issue">New Issue</a>
+  <a href="logout.php" id="logout">Logout</a>
+</nav>
+
 <main>
-  <form action="home.php" method="POST">
+  <form id="login-form" action="home.php" method="POST">
     <h1>Login</h1>
     <p>Enter your email and password</p>
       <label for="email"> Email</label>
@@ -18,7 +27,7 @@
       <input id="password" type="text" name="password" class="input" /> 
       <br>
       <br>            
-      <button type="submit" class="btn" id = "btn" name="login"> Submit</button>
+      <button type="submit" class="btn" id = "login-submit" name="login"> Submit</button>
   </form>
 </main>
 
