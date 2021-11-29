0d7184f142dd704ad0984ed0aa13cad88c13737e
Omari Brown <Omaribrown16@gmail.com>
2021-11-29 14:18:48 -0500

    First integration test with HTML.Main/Context box needs fixing.

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
 "use strict";
+// <script src="script.js"></script>
+//Add in the head tag in index
 
 window.onload = function()
 {
     console.log("Window loaded");
-    var mainbox = document.getElementById("main");
+    var contentbox = document.getElementById("content");
+    console.log(contentbox);
+    var myformtest=contentbox.getElementById("login-form");
+    console.log(myformtest);
+    var myinputtest=contentbox.getElementByTagName("input");
+    console.log(myinputtest);
+    console.log(myinputtest[0]); 
     var navigation = document.getElementById("left-sidebar");
+    console.log(navigation);
     var anchorlist = navigation.getElementsByTagName("a");
     console.log(anchorlist);
     Array.from(anchorlist).forEach(function (anchor)
     {
         anchor.addEventListener("click", function(event)
         {
-            event.preventDefault;
+            event.preventDefault();
             var link_source = event.target.href;
             console.log(link_source);
-            if (link_source=="home.php")
+            if (link_source=="http://localhost/info2180-finalproject/home.php")
             {
                 fetch("home.php")
                     .then(console.log("Fetching"))
                     .then(response => response.text())
-                    .then(data => mainbox.innerHTML = data)
+                    .then(data => contentbox.innerHTML = data)
                     .catch(error => 
                     {
                         console.log("There was an error");
                         console.log(error);
                     });
-                var button_issue = mainbox.getElementById("top").getElementsByTagName("button")[0];
+                var top = contentbox.getElementById("top");
+                var button_issue = top.getElementByTagName("a")[0];
+                console.log("button_issue");
                 button_issue.addEventListener("click", function()
                 {
                     var link_issue = document.getElementById('issue');
                     link_issue.click();
                 });
-                var description_links = mainbox.getElementsByTagName("a");
+                var description_links = contentbox.getElementsByTagName("a");
                 Array.from(description_links).forEach(function (description_link)
                 {
                     description_link.addEventListener("click", function(event)
@@ -39,13 +50,13 @@ window.onload = function()
                         fetch("description.php")
                             .then(console.log("Fetching"))
                             .then(response => response.text())
-                            .then(data => mainbox.innerHTML = data)
+                            .then(data => contentbox.innerHTML = data)
                             .catch(error => 
                             {
                                 console.log("There was an error");
                                 console.log(error);
                             });
-                        var description_buttons = mainbox.getElementsByTagName("button");
+                        var description_buttons = contentbox.getElementsByTagName("button");
                         var markedClosed = description_buttons[0];
                         var markedInProgress = description_buttons[1];
                         markedClosed.addEventListener("click", function()
@@ -78,7 +89,7 @@ window.onload = function()
                         fetch(`filename.php?filter=${event.target.id}`)
                             .then(console.log("Fetching"))
                             .then(response => response.text())
-                            .then(data => mainbox.innerHTML = data)
+                            .then(data => contentbox.innerHTML = data)
                             .catch(error => 
                             {
                                 console.log("There was an error");
@@ -91,7 +102,7 @@ window.onload = function()
                 });
                 console.log("Home page loaded");
             }
-            else if(link_source=="user.php")
+            else if(link_source=="http://localhost/info2180-finalproject/user.php")
             {
                 //how do I check if user is the administrator
                 //diable link if not the administrator
@@ -99,22 +110,22 @@ window.onload = function()
                 fetch("user.php")
                     .then(console.log("Fetching"))
                     .then(response => response.text())
-                    .then(data => mainbox.innerHTML = data)
+                    .then(data => contentbox.innerHTML = data)
                     .catch(error => 
                     {
                         console.log("There was an error");
                         console.log(error);
                     });
-                var form = mainbox.getElementById("");
+                var form = contentbox.getElementById("new-user-form");
                 form.addEventListener('submit', function (element) 
                 {
-                    var firstname = form.getElementById("").value.trim();
-                    var lastname = form.getElementById("").value.trim();
-                    var password = form.getElementById("").value.trim();
-                    var email = form.getElementById("").value.trim();
+                    var firstname = form.getElementById("fname").value.trim();
+                    var lastname = form.getElementById("lname").value.trim();
+                    var password = form.getElementById("password").value.trim();
+                    var email = form.getElementById("email").value.trim();
                     if (isEmpty(firstname) || isEmpty(lastname) || isEmpty(password) || isEmpty(email))
                     {
-                        myform.getElementById("message").innertext = "Please complete the form before submitting.";
+                        myform.getElementById("mew-user-message").innertext = "Please complete the form before submitting.";
                         element.preventDefault();
                     }
                     else
@@ -131,29 +142,29 @@ window.onload = function()
                 });
                 console.log("Add User page loaded");
             }
-            else if (link_source=="create.php")
+            else if (link_source=="http://localhost/info2180-finalproject/create.php")
             {
                 fetch("create.php")
                     .then(console.log("Fetching"))
                     .then(response => response.text())
-                    .then(data => mainbox.innerHTML = data)
+                    .then(data => contentbox.innerHTML = data)
                     .catch(error => 
                     {
                         console.log("There was an error");
                         console.log(error);
                     });
-                var form = mainbox.getElementById("")
+                var form = contentbox.getElementById("create-issue-form")
                 form.addEventListener('submit', function (element) 
                 {
                     //How do we update AssignedTo with list of all current users
-                    var title = form.getElementById("").value.trim();
-                    var description = form.getElementById("").value.trim();
+                    var title = form.getElementById("title").value.trim();
+                    var description = form.getElementById("description").value.trim();
                     var assignedTo = form.getElementById("").value
-                    var type = form.getElementById("").value;
-                    var priority = form.getElementById("").value;
+                    var type = form.getElementById("assigned").value;
+                    var priority = form.getElementById("priority").value;
                     if (isEmpty(title) || isEmpty(description) || isEmpty(assignedTo) || isEmpty(type) || isEmpty(priority))
                     {
-                        myform.getElementById("message").innertext = "Please complete the form before submitting.";
+                        myform.getElementById("create-issue-message").innertext = "Please complete the form before submitting.";
                         element.preventDefault();
                     }
                     else
@@ -163,27 +174,28 @@ window.onload = function()
                 });
                 console.log("Create Issue page loaded");
             }
-            else (link_source=="logout.php")
+            else (link_source=="http://localhost/info2180-finalproject/logout.php")
             {
                 fetch("logout.php")
                     .then(console.log("Fetching"))
                     .then(response => response.text())
-                    .then(data => mainbox.innerHTML = data)
+                    .then(data => contentbox.innerHTML = data)
                     .catch(error => 
                     {
                         console.log("There was an error");
                         console.log(error);
                     });
                 console.log("Session terminated.");
-                fetch("login.php")
+                //I probably dont need this
+                /*fetch("login.php")
                     .then(console.log("Fetching"))
                     .then(response => response.text())
-                    .then(data => mainbox.innerHTML = data)
+                    .then(data => contentbox.innerHTML = data)
                     .catch(error => 
                     {
                         console.log("There was an error");
                         console.log(error);
-                    });
+                    });*/
                 console.log("Login page loaded");
                 navigation.classList.add("hidden");
                 // What else needs to be done?
@@ -193,9 +205,11 @@ window.onload = function()
     });
     var link_home = document.getElementById("home"); 
     navigation.classList.add("hidden");
+    console.log("navigation hidden");
     var loginbutton = document.getElementById("login-submit");
     loginbutton.addEventListener("click", function()
     {
+        event.preventDefault(event);
         var email = document.getElementById("email").value.trim();
         var password = document.getElementById("password").value.trim();
         if(checkLogin(email, password))
@@ -207,7 +221,9 @@ window.onload = function()
         else
         {
             console.log("User login not valid.");
-            element.preventDefault();
+            let message=document.getElementById("login-form-message");
+            message.innerText="Invalid Login."
+            event.preventDefault();
         }
     });
 }
