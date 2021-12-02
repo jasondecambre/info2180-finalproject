413096227e51605ba7e095fae895b74dcce69fbb
Jason DeCambre <jasondecambre@gmail.com>
2021-12-01 23:09:41 -0500

    extra touches

--- script.js
+++ script.js
@@ -236,7 +236,7 @@ window.onload = function()
 function checkLogin(email, password)
 {
     var emailexpression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var passwordexpression = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    var passwordexpression = /^(?=.*[A-Za-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
     if (emailexpression.test(email))
     {
         console.log("Valid email");
